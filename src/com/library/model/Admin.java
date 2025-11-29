bf6d7effa124465350cea0ef2e1e65eecc195bcb
AkshayDev-Ops-28 <akshayvaidya2024@gmail.com>
2025-11-30 01:31:20 +0530

    Add Admin class with login functionality

---
+++ src/com/library/model/Admin.java
@@ -0,0 +1,8 @@
+public class Admin {
+    private String username = "admin";
+    private String password = "1234";
+
+    public boolean login(String user, String pass) {
+        return user.equals(username) && pass.equals(password);
+    }
+}
